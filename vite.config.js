b7cb3cf49ae51cd428747560385d0d612b0a52db
ZefanyaDelvin <delvinzefanya@gmail.com>
2025-06-10 21:35:41 +0700

    Updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Delvin-portfolio/',
   plugins: [react(),
     tailwindcss(),
   ],
